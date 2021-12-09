68540cb40a68f4f24a43091240c114ea0071a069
jun26 <jun26@DESKTOP-PER5GRP>
2021-12-09 23:09:13 +0900

    init project

---
+++ src/edu/Main.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package edu;
+
+/**
+ * @author jun26
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 변수 선언하기 
+		int firstNo, secondNo;
+		int sum;
+		// 두 수 입력하기
+		firstNo = 10;
+		secondNo = 20;
+		// 합계내기(C= A+B)
+		sum = firstNo+secondNo;
+		//합계 출력하기
+		System.out.println("합계: "+ sum);
+	}
+
+}
